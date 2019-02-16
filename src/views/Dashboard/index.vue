f1355eb42b58ce47925be26911bba10875cfcc2e
linhao0112 <15067653377@163.com>
2019-02-16 20:13:28 +0800

    Update index.vue

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -75,14 +75,13 @@
 					<Col span='14'>
 					<draggable :options="{animation: 60,group:'panel'}">
 						<!-- Map box -->
-						  <div class="box box-solid bg-light-blue-gradient" v-if="visitor">
+						<div class="box box-solid bg-light-blue-gradient" v-if="visitor">
 							<div class="box-header">
 							  <!-- tools box -->
 							  <div class="pull-right box-tools">
 								  <button type="button" class="btn btn-primary btn-sm pull-right" @click="visitor=!visitor"><i class="fa fa-times"></i>
 								  </button>
-								<button type="button" class="btn btn-primary btn-sm daterange pull-right" data-toggle="tooltip" style="margin-right: 5px;"
-										title="Date range">
+								<button type="button" class="btn btn-primary btn-sm daterange pull-right" data-toggle="tooltip" style="margin-right: 5px;" title="Date range">
 								  <i class="fa fa-calendar"></i></button>
 								<button type="button" class="btn btn-primary btn-sm pull-right" title="Collapse" style="margin-right: 5px;" @click="visitorbody=!visitorbody">
 								  <i class="fa fa-minus" v-if="visitorbody"></i> <i class="fa fa-plus" v-if="!visitorbody"></i>
@@ -104,7 +103,7 @@
 						  </div>
 						  <!-- /.box -->
 						<!-- Chat box -->
-						<div class="box box-success">
+						<div class="box box-success" v-if="chat">
 							<div class="box-header">
 								<i class="fa fa-comments-o"></i>
 
@@ -112,13 +111,17 @@
 
 								<div class="box-tools pull-right" data-toggle="tooltip" title="Status">
 									<div class="btn-group" data-toggle="btn-toggle">
-										<button type="button" class="btn btn-default btn-sm active"><i class="fa fa-square text-green"></i>
+										<button type="button" class="btn btn-success btn-sm" style="margin-right: 5px;" @click="chatbody=!chatbody">
+											<i class="fa fa-minus" v-if="chatbody"></i>
+											<i class="fa fa-plus" v-if="!chatbody"></i>
 										</button>
-										<button type="button" class="btn btn-default btn-sm"><i class="fa fa-square text-red"></i></button>
+										<button type="button" class="btn btn-success btn-sm" @click="chat=false"><i class="fa fa-times"></i>
+										</button>
+									
 									</div>
 								</div>
 							</div>
-							<div class="box-body chat" id="chat-box">
+							<div class="box-body chat" id="chat-box" v-if="chatbody">
 								<!-- chat item -->
 								<div class="item">
 									<img src="../../assets/img/user4-128x128.jpg" alt="user image" class="online">
@@ -178,9 +181,6 @@
 									</div>
 								</div>
 								<!-- /.item -->
-							</div>
-							<!-- /.chat -->
-							<div class="box-footer">
 								<div class="input-group">
 									<input class="form-control" placeholder="Type message...">
 
@@ -189,17 +189,27 @@
 									</div>
 								</div>
 							</div>
+							<!-- /.chat -->
 						</div>
 						<!-- /.box (chat box) -->
 
 						<!-- TO DO List -->
-						<div class="box box-primary">
+						<div class="box box-primary" v-if="progress">
 							<div class="box-header">
 								<i class="ion ion-clipboard"></i>
 								<h3 class="box-title">故障电梯处理进程</h3>
+								<div class="pull-right box-tools">
+									<button type="button" class="btn btn-primary btn-sm pull-right" @click="progress=!progress"><i class="fa fa-times"></i>
+									</button>
+								<button type="button" class="btn btn-primary btn-sm daterange pull-right" data-toggle="tooltip" style="margin-right: 5px;" title="Date range">
+									<i class="fa fa-calendar"></i></button>
+								<button type="button" class="btn btn-primary btn-sm pull-right" title="Collapse" style="margin-right: 5px;" @click="progressbody=!progressbody">
+									<i class="fa fa-minus" v-if="progressbody"></i> <i class="fa fa-plus" v-if="!progressbody"></i>
+								</button>
+								</div>
 							</div>
 							<!-- /.box-header -->
-							<div class="box-body">
+							<div class="box-body" v-if="progressbody">
 								<!-- See dist/js/pages/dashboard.js to activate the todoList plugin -->
 								<ul class="todo-list">
 									<li v-for="item in todo">
@@ -216,11 +226,7 @@
 									</li>
 								</ul>
 							</div>
-							&laquo; &raquo;
 							<!-- /.box-body -->
-							<div class="box-footer clearfix no-border">
-								<button type="button" class="btn btn-default pull-right"><i class="fa fa-plus"></i> Add item</button>
-							</div>
 						</div>
 						<!-- /.box -->
 
@@ -263,48 +269,59 @@
 					<Col span="10">
 						<draggable :options="{animation: 60,group:'panel'}">
 						<!-- solid sales graph -->
-						<div class="box box-solid bg-teal-gradient">
+						<div class="box box-solid bg-teal-gradient" v-if="chart">
 							<div class="box-header">
 								<i class="fa fa-th"></i>
 
 								<h3 class="box-title">业务图表</h3>
-
+								
 								<div class="box-tools pull-right">
-									<button type="button" class="btn bg-teal btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
+									<button type="button" class="btn bg-teal btn-sm" @click="chartbody=!chartbody">
+										<i class="fa fa-minus" v-if="chartbody"></i>
+										<i class="fa fa-plus" v-if="!chartbody"></i>
 									</button>
-									<button type="button" class="btn bg-teal btn-sm" data-widget="remove"><i class="fa fa-times"></i>
+									<button type="button" class="btn bg-teal btn-sm" @click="chart=false"><i class="fa fa-times"></i>
 									</button>
 								</div>
 							</div>
-							<div class="box-body border-radius-none">
-								<div style=''>
-								<div id="line" style="height: 200px; width:80%"></div>
-								</div>
+							<div class="box-body border-radius-none" v-if="chartbody">
+								<swiper id="swiperBox" v-bind:options="swiperOption" ref="mySwiper">
+									<swiper-slide>
+										<div style=''>
+										<div id="line" style="height: 200px; width:80%"></div>
+										</div>
+									</swiper-slide>
+									<swiper-slide >
+										<div style=''>
+										<div id="circle" style="height: 200px; width:80%"></div>
+										</div>
+									</swiper-slide>
+								</swiper>
 							</div>
 							<!-- /.box-body -->
-							<div class="box-footer no-border">
+<!-- 							<div class="box-footer no-border">
 								<div class="row">
 									<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
 										<input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60" data-fgColor="#39CCCC">
 
 										<div class="knob-label">Mail-Orders</div>
 									</div>
-									<!-- ./col -->
+
 									<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
 										<input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60" data-fgColor="#39CCCC">
 
 										<div class="knob-label">Online</div>
 									</div>
-									<!-- ./col -->
+
 									<div class="col-xs-4 text-center">
 										<input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60" data-fgColor="#39CCCC">
 
 										<div class="knob-label">In-Store</div>
 									</div>
-									<!-- ./col -->
+
 								</div>
-								<!-- /.row -->
-							</div>
+
+							</div> -->
 							<!-- /.box-footer -->
 						</div>
 						<!-- /.box -->
@@ -393,8 +410,11 @@
 			</section>
 			<Poptip trigger="hover" placement="left" style="margin-top:10px;margin-left:1600px;" width="50">
 				<div style="cursor: pointer;color:#0000FF">显示内容</div>
-				<div class="api" slot="content">
-					<checkbox v-model="visitor">map</checkbox>
+				<div slot="content" style="width:100px;height:300px">
+					<div><checkbox v-model="visitor">Map</checkbox></div>
+					<div><checkbox v-model="chat">客户意见反馈</checkbox></div>
+					<div><checkbox v-model="progress">故障处理进程</checkbox></div>
+					<div><checkbox v-model="chart">业务图表</checkbox></div>
 				</div>
 			</Poptip>
 	</div>
@@ -415,123 +435,206 @@
 			return {
 				msg: 'Welcome to Your Vue.js App',
 				visitor: true,
+				chat: true,
+				chart: true,
+				progress: true,
 				visitorbody: true,
+				chatbody: true,
+				chartbody: true,
+				progressbody: true,
 				shine:true,
 				todo:[{pro:"江南一号",explain:"电梯通信异常，经排查开关电源盒损坏。预计明天购买开关电源盒，恢复电梯正常使用。"},
 						{pro:"上海科技大学",explain:"电梯在使用时光幕异常。经排查，光幕接收信号异常。预计后天购买光幕，恢复电梯正常使用。"},
 						{pro:"江南一号",explain:"电梯通信异常，经排查开关电源盒损坏。预计明天购买开关电源盒，恢复电梯正常使用。"},
 				],
+				swiperOption:{
+					autoplay:true,
+					delay:10000,
+					notNextTick:true,
+					direction:'horizontal',
+					grabCursor:true,
+					setWrapperSize:true,
+					autoHerght:true,
+					slidesPerView: 1,
+					mousewheel:true,
+					mousewheelControl:true,
+					height:window.innerHeight,
+					resistanceRatio:0,
+					observeParents:true,
+					observer:true,//修改swiper自己或子元素时，自动初始化swiper
+					allowTouchMove: false,
+					navigation: {
+						prevEl: '.swiper-button-prev',
+						nextEl: '.swiper-button-next',
+					},
+				},
 				option1:{
-									title: {
-											// text: '折线图堆叠'
-									},
-									tooltip: {
-											trigger: 'axis'
-									},
-									legend: {
-											data:['数据1','数据2','数据3','数据4','数据5'],
-											textStyle: {
-											            color: '#FF7F00',
-											            fontSize:'16'
-											            },
-									},
-									grid: {
-											left: '3%',
-											right: '4%',
-											bottom: '3%',
-											containLabel: true
-									},
-									toolbox: {
-											feature: {
-													// saveAsImage: {}
+					title: {
+							// text: '折线图堆叠'
+					},
+					tooltip: {
+							trigger: 'axis'
+					},
+					legend: {
+							data:['数据1','数据2','数据3','数据4','数据5'],
+							textStyle: {
+							            color: '#FF7F00',
+							            fontSize:'16'
+							            },
+					},
+					grid: {
+							left: '3%',
+							right: '4%',
+							bottom: '3%',
+							containLabel: true
+					},
+					toolbox: {
+							feature: {
+									// saveAsImage: {}
+							}
+					},
+					xAxis: {
+							type: 'category',
+							boundaryGap: false,
+							color:'#f0f',
+							data: ['周一','周二','周三','周四','周五','周六','周日'],
+										splitLine: {
+               			show: false, 
+                    //  改变轴线颜色
+                    lineStyle: {
+                        // 使用深浅的间隔色
+                        color: ['red']
+                        }                            
+                    },
+                    //  改变x轴颜色
+                    axisLine:{
+                        lineStyle:{
+                            color:'#df0af8',
+//                            width:8,//这里是为了突出显示加上的，可以去掉
+                        }
+                    },                         
+                    //  改变x轴字体颜色和大小
+                    axisLabel: {
+                        textStyle: {
+                            color: '#FF7F00',
+                            fontSize:'16'
+                        },
+                    },
+					},
+					yAxis: {
+							type: 'value',
+							splitLine: {
+							               			show: false, 
+							                    //  改变轴线颜色
+							                    lineStyle: {
+							                        // 使用深浅的间隔色
+							                        color: ['white']
+							                        }                            
+							                    },
+							                    //  改变x轴颜色
+							                    axisLine:{
+							                        lineStyle:{
+							                            color:'#df0af8',
+							//                            width:8,//这里是为了突出显示加上的，可以去掉
+							                        }
+							                    },                         
+							                    //  改变x轴字体颜色和大小
+							                    axisLabel: {
+							                        textStyle: {
+							                            color: '#FF7F00',
+							                            fontSize:'16'
+							                        },
+							                    },
+					},
+					series: [
+							{
+									name:'数据1',
+									type:'line',
+									stack: '总量',
+									data:[120, 132, 101, 134, 90, 230, 210]
+							},
+							{
+									name:'数据2',
+									type:'line',
+									stack: '总量',
+									data:[220, 182, 191, 234, 290, 330, 310]
+							},
+							{
+									name:'数据3',
+									type:'line',
+									stack: '总量',
+									data:[150, 232, 201, 154, 190, 330, 410]
+							},
+							{
+									name:'数据4',
+									type:'line',
+									stack: '总量',
+									data:[320, 332, 301, 334, 390, 330, 320]
+							},
+							{
+									name:'数据5',
+									type:'line',
+									stack: '总量',
+									data:[820, 932, 901, 934, 1290, 1330, 1320]
+							}
+					]
+				},
+				option4:{
+					title : {
+							text: '用户来源',
+							x:'center',
+							textStyle: {
+		                	color: '#FF7F00',
+		                 	fontSize:'16'
+		             		},
+					},
+					tooltip : {
+							trigger: 'item',
+							formatter: "{a} <br/>{b} : {c} ({d}%)"
+					},
+					legend: {
+							orient : 'vertical',
+							x : 'left',
+							data:['上海','宁波','杭州','南京','其他']
+					},
+					toolbox: {
+							show : false,
+							feature : {
+									mark : {show: true},
+									dataView : {show: true, readOnly: false},
+									magicType : {
+											show: true, 
+											type: ['pie', 'funnel'],
+											option: {
+													funnel: {
+															x: '25%',
+															width: '50%',
+															funnelAlign: 'left',
+															max: 1548
+													}
 											}
 									},
-									xAxis: {
-											type: 'category',
-											boundaryGap: false,
-											color:'#f0f',
-											data: ['周一','周二','周三','周四','周五','周六','周日'],
-														splitLine: {
-				               			show: false, 
-				                    //  改变轴线颜色
-				                    lineStyle: {
-				                        // 使用深浅的间隔色
-				                        color: ['red']
-				                        }                            
-				                    },
-				                    //  改变x轴颜色
-				                    axisLine:{
-				                        lineStyle:{
-				                            color:'#df0af8',
-				//                            width:8,//这里是为了突出显示加上的，可以去掉
-				                        }
-				                    },                         
-				                    //  改变x轴字体颜色和大小
-				                    axisLabel: {
-				                        textStyle: {
-				                            color: '#FF7F00',
-				                            fontSize:'16'
-				                        },
-				                    },
-									},
-									yAxis: {
-											type: 'value',
-											splitLine: {
-											               			show: false, 
-											                    //  改变轴线颜色
-											                    lineStyle: {
-											                        // 使用深浅的间隔色
-											                        color: ['white']
-											                        }                            
-											                    },
-											                    //  改变x轴颜色
-											                    axisLine:{
-											                        lineStyle:{
-											                            color:'#df0af8',
-											//                            width:8,//这里是为了突出显示加上的，可以去掉
-											                        }
-											                    },                         
-											                    //  改变x轴字体颜色和大小
-											                    axisLabel: {
-											                        textStyle: {
-											                            color: '#FF7F00',
-											                            fontSize:'16'
-											                        },
-											                    },
-									},
-									series: [
-											{
-													name:'数据1',
-													type:'line',
-													stack: '总量',
-													data:[120, 132, 101, 134, 90, 230, 210]
-											},
-											{
-													name:'数据2',
-													type:'line',
-													stack: '总量',
-													data:[220, 182, 191, 234, 290, 330, 310]
-											},
-											{
-													name:'数据3',
-													type:'line',
-													stack: '总量',
-													data:[150, 232, 201, 154, 190, 330, 410]
-											},
-											{
-													name:'数据4',
-													type:'line',
-													stack: '总量',
-													data:[320, 332, 301, 334, 390, 330, 320]
-											},
-											{
-													name:'数据5',
-													type:'line',
-													stack: '总量',
-													data:[820, 932, 901, 934, 1290, 1330, 1320]
-											}
+									restore : {show: true},
+									saveAsImage : {show: true}
+							}
+					},
+					calculable : true,
+					series : [
+							{
+									name:'用户来源',
+									type:'pie',
+									radius : '55%',
+									center: ['50%', '60%'],
+									data:[
+											{value:1548, name:'上海'},
+											{value:335, name:'宁波'},
+											{value:310, name:'杭州'},
+											{value:234, name:'南京'},
+											{value:135, name:'其他'}
 									]
-								},
+							}
+					]
+				},
 			}
 		},
 		mounted(){
@@ -542,6 +645,8 @@
 			draw(){
 				var myChart = echarts.init(document.getElementById('line'));
 				myChart.setOption(this.option1);
+				var myChart4 = echarts.init(document.getElementById('circle'));
+				myChart4.setOption(this.option4);
 			},
 			shineword(){
 				setTimeout(()=>{
