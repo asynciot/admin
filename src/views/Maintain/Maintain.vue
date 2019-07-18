02f4ba79eba719982509414862fbd8d80d994523
linhao0112 <15067653377@163.com>
2019-07-18 08:36:15 +0800

    7-18

--- src/views/Maintain/Maintain.vue
+++ src/views/Maintain/Maintain.vue
@@ -283,7 +283,6 @@
 				this.loading = true
 				this.counter = 0
 				let res = await this.$api.fault(this.options)
-				alert(123)
 				if (res.data.code === 0) {
 					this.data2 = res.data.data.list
 					this.options.total = res.data.data.totalNumber
