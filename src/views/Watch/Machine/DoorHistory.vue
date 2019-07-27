150df3c3e3ac39ee454e0bad736b3ede3b74402f
linhao0112 <15067653377@163.com>
2019-07-27 13:57:20 +0800

    123

--- src/views/Watch/Machine/DoorHistory.vue
+++ src/views/Watch/Machine/DoorHistory.vue
@@ -70,6 +70,7 @@ div.layout-content-main
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 							div.doorbox(style="right:-0%" id='rightdoor')
 =======
 =======
@@ -77,6 +78,9 @@ div.layout-content-main
 								p|光幕信号
 							div.doorbox(:style="{ right: `-${(show.position / doorWidth) * 50}%` }")
 >>>>>>> parent of 37ad0bb... 7-17
+=======
+							div.doorbox(:style="{ right: `-${(show.position / doorWidth) * 50}%` }")
+>>>>>>> parent of 3ea2754... 7-27
 =======
 							div.doorbox(:style="{ right: `-${(show.position / doorWidth) * 50}%` }")
 >>>>>>> parent of 3ea2754... 7-27
