1e17484bb412fa315875f4db751907910eb166ee
Ternado <291699947@qq.com>
2019-10-19 15:57:49 +0800

    Revert "123"
    This reverts commit 49d8078f4815e014634ff05317bb23597d4074d8.

--- src/router/index.js
+++ src/router/index.js
@@ -58,7 +58,6 @@ const MaintainList = resolve => require(['@/views/Maintain/MaintainList'], resol
 const FaultRank = resolve => require(['@/views/Maintain/FaultRank'], resolve)
 const Maintain = resolve => require(['@/views/Maintain/Maintain'], resolve)
 const Finish = resolve => require(['@/views/Maintain/Finish'], resolve)
-const Treated = resolve => require(['@/views/Maintain/Treated'], resolve)
 const Order = resolve => require(['@/views/Maintain/Order'], resolve)
 const AuditingList = resolve => require(['@/views/Maintain/AuditingList'], resolve)
 const OrderAdopt = resolve => require(['@/views/Maintain/OrderAdopt'], resolve)
@@ -353,14 +352,6 @@ export default new Router({
 					},
 					component: Finish
 				},
-				{
-					path: '/maintain/treated/:id',
-					name: 'treated',
-					meta: {
-						name: '完成工单'
-					},
-					component: Treated
-				},
 				//单位管理
 				//维保单位
 				{
@@ -518,7 +509,7 @@ export default new Router({
 						name: '版本更新'
 					},
 					component: Evolution
-				},
+				}, 
 				{
 					path: '/information/list',
 					name: 'alList',
--- src/views/Maintain/Finish.vue
+++ src/views/Maintain/Finish.vue
@@ -12,31 +12,31 @@
 										Col(span="8" style="margin-top:10px")|{{$t('order ID')}}:{{list.order_id}}
 										Col(span="8" style="margin-top:10px")|{{$t('device name')}}:{{list.device_name}}
 										Col(span="8" style="margin-top:10px")|IMEI:{{list.IMEI}}
-										Col(span="8" style="margin-top:10px" v-show="list.state == 'treated'")|{{$t('state')}}:{{list.result}}
-										Col(span="8" style="margin-top:10px" v-show="list.state == 'untreated'")|{{$t('state')}}:{{$t('treating')}}
+										Col(span="8" style="margin-top:10px" v-if="list.state == 'treated'")|{{$t('state')}}:{{list.result}}
+										Col(span="8" style="margin-top:10px" v-if="list.state == 'untreated'")|{{$t('state')}}:{{$t('treating')}}
 										Col(span="8" style="margin-top:10px")|{{$t('confirm time')}}:{{list.confirm_time}}
 										Col(span='24' style="margin-top:10px")
 											textarea(v-model='ps' style="width:100%;height:60px", :placeholder="$t('Description of maintenance')")
 									Col(span=12 style="margin-top:10px")|{{$t('photo before treating')}}:
 									Col(span=12 style="margin-top:10px")|{{$t('photo after treating')}}:
 									Col(span=12 style='height: 160px')
-										upload(:before-upload='before')
-											img(id="before" src='../../assets/add.jpg' style="height:130px; width:80%; cursor: pointer;")
+										upload(:before-upload='before1')
+											img(id="before1" src='../../assets/add.jpg' style="height:130px; width:80%; cursor: pointer;")
 									Col(span=12 style='height: 160px')
-										upload(:before-upload='after')
-											img(id="after" src='../../assets/add.jpg' style="height:130px; width:80%; cursor: pointer;")
+										upload(:before-upload='after1')
+											img(id="after1" src='../../assets/add.jpg' style="height:130px; width:80%; cursor: pointer;")
 						Col(span=24 style="margin-top: 10px")
 							Col(span=6 align="center")
-								Button(type="success",@click="finish('finish')" disabled v-show="list.state == 'treated'")|{{list.result}}
-								Button(type="success",@click="finish('finish')" v-show="((list.state != 'treated')&&(!sent))" disabled='false')|{{$t('complete')}}
-								Button(type="success",@click="finish('finish')" v-show="((list.state != 'treated')&&(sent))")|{{$t('complete')}}
+								Button(type="success",@click="finish('finish')" disabled v-if="list.state == 'treated'")|{{list.result}}
+								Button(type="success",@click="finish('finish')" v-if="((list.state != 'treated')&&(!sent))" disabled='false')|{{$t('complete')}}
+								Button(type="success",@click="finish('finish')" v-if="((list.state != 'treated')&&(sent))")|{{$t('complete')}}
 							Col(span=6 align="center")
 								Button(type="primary",@click="examine()",v-if="dispatch != true" disabled='false')|{{$t('reprieve')}}
 								Button(type="primary",@click="examine()",v-else)|{{$t('reprieve')}}
 							Col(span=6 align="center")
-								Button(type="warning",@click="finish('transfer')" disabled v-show="list.state == 'treated'")|{{list.result}}
-								Button(type="warning",@click="finish('transfer')" v-show="((list.state != 'treated')&&(!sent))" disabled='false')|{{$t('transfer')}}
-								Button(type="warning",@click="finish('transfer')" v-show="((list.state != 'treated')&&(sent))")|{{$t('transfer')}}
+								Button(type="warning",@click="finish('transfer')" disabled v-if="list.state == 'treated'")|{{list.result}}
+								Button(type="warning",@click="finish('transfer')" v-if="((list.state != 'treated')&&(!sent))" disabled='false')|{{$t('transfer')}}
+								Button(type="warning",@click="finish('transfer')" v-if="((list.state != 'treated')&&(sent))")|{{$t('transfer')}}
 							Col(span=6 align='center')
 								Button(@click="$router.back(-1)")|{{$t('cancel')}}
 </template>
@@ -45,7 +45,7 @@
 	export default{	
 		data(){
 			return{
-				sent:true,
+				sent:false,
 				username:this.global.username,
 				id:window.localStorage.getItem('id'),
 				fault:[0,0,0,0,0,0,0,0],
@@ -53,7 +53,11 @@
 					type:'1',
 				},
 				beforefile1:'',
+				beforefile2:'',
+				beforefile3:'',
 				afterfile1:'',
+				afterfile2:'',
+				afterfile3:'',
 				faultcode:false,
 				list:[],
 				query:{
@@ -70,7 +74,7 @@
 				file:'',
 				filename:'',
 				ps:'',
-				dispatch:this.global.functions.work_dispatch,
+				// dispatch:this.global.functions.work_dispatch,
 			}
 		},
 		computed: {
@@ -85,7 +89,8 @@
 			code(){
 				if (this.form.type == '1'){
 					this.faultcode=false;
-				}else {
+				}
+				else {
 					this.faultcode=true;
 				}
 			},
@@ -97,20 +102,14 @@
 					res.data.data.list[0].IMEI = ech.data.data.list[0].IMEI
 					res.data.data.list[0].install_addr = ech.data.data.list[0].install_addr
 					res.data.data.list[0].cell_address = ech.data.data.list[0].cell_address
-					if (res.data.data.list[0].result == 'transfer') {
-						res.data.data.list[0].result=this.$t('transferred')
-					}else {
-						res.data.data.list[0].result=this.$t('treated')
-					}
+					if (res.data.data.list[0].result == 'transfer') {res.data.data.list[0].result=this.$t('transferred')}
+					else {res.data.data.list[0].result=this.$t('treated')}
 					this.list = res.data.data.list[0]
-<<<<<<< HEAD
-=======
 					
->>>>>>> parent of 67680db... 1111
 					var before=this.list.before_pic.split(';')
 					var after=this.list.after_pic.split(';')
-					document.getElementById('before').src='http://server.asynciot.com/getfile?filePath='+before[0];
-					document.getElementById('after').src='http://server.asynciot.com/getfile?filePath='+after[0];
+					document.getElementById('before1').src='http://server.asynciot.com/getfile?filePath='+before[0];
+					document.getElementById('after1').src='http://server.asynciot.com/getfile?filePath='+after[0];
 				} else {
 					this.$Notice.error({
 						title: this.$t('error'),
@@ -118,7 +117,84 @@
 					});
 				}
 			},
-			before (file) {
+			before1 (file) {
+				if (this.list.state != "treated"){
+					var type = file.name.split('.')
+					if ((type[1] == 'png')||(type[1] == 'gif')||(type[1] == 'jpg')||(type[1] == 'bmp')||(type[1] == 'jpeg')){
+					if (file.size>2097000) {
+						this.$Notice.warning({
+							title: this.$t('warning'),
+							desc: this.$t('File size must be less than 2M')
+						})
+					}
+					else {
+
+					this.beforefile1 = new File([file], 'before'+file.name,{type:"image/jpeg"});
+					let url = null;
+					if (window.createObjectURL!=undefined) { // basic
+						url = window.createObjectURL(this.beforefile1) ;
+					}else if (window.webkitURL!=undefined) { // webkit or chrome
+						url = window.webkitURL.createObjectURL(this.beforefile1) ;
+					}else if (window.URL!=undefined) { // mozilla(firefox)
+						url = window.URL.createObjectURL(this.beforefile1) ;
+					}
+					document.getElementById('before1').src=url;
+					return false;
+					}
+					}
+					else{
+						this.$Notice.warning({
+							title: this.$t('warning'),
+							desc: this.$t('File type must be picture')
+						})
+					}
+				}
+				else{
+					this.$Notice.warning({
+						title: this.$t('warning'),
+						desc: this.$t('Can not supplementary picture.')
+					})
+				}
+			},
+			before2 (file) {
+				if (this.list.state != "treated"){
+				var type = file.name.split('.')
+				if ((type[1] == 'png')||(type[1] == 'gif')||(type[1] == 'jpg')||(type[1] == 'bmp')||(type[1] == 'jpeg')){
+				this.beforefile2 = new File([file], 'before'+file.name,{type:"image/jpeg"});
+				if (file.size>2097000) {
+					this.$Notice.warning({
+						title: this.$t('warning'),
+						desc: this.$t('File size must be less than 2M')
+					})
+				}
+				else {
+				let url = null;
+				if (window.createObjectURL!=undefined) { // basic
+					url = window.createObjectURL(this.beforefile2) ;
+				}else if (window.webkitURL!=undefined) { // webkit or chrome
+					url = window.webkitURL.createObjectURL(this.beforefile2) ;
+				}else if (window.URL!=undefined) { // mozilla(firefox)
+					url = window.URL.createObjectURL(this.beforefile2) ;
+				}
+				document.getElementById('before2').src=url;
+				return false;
+				}
+				}
+				else{
+					this.$Notice.warning({
+						title: this.$t('warning'),
+						desc: this.$t('File type must be picture')
+					})
+				}
+				}
+				else{
+					this.$Notice.warning({
+						title: this.$t('warning'),
+						desc: this.$t('Can not supplementary picture.')
+					})
+				}
+			},
+			before3 (file) {
 				if (this.list.state != "treated"){
 					var type = file.name.split('.')
 					if ((type[1] == 'png')||(type[1] == 'gif')||(type[1] == 'jpg')||(type[1] == 'bmp')||(type[1] == 'jpeg')){
@@ -127,18 +203,17 @@
 								title: this.$t('warning'),
 								desc: this.$t('File size must be less than 2M')
 							})
-						}
-						else {
-							this.beforefile1 = new File([file], 'before'+file.name,{type:"image/jpeg"});
+						}else {
+							this.beforefile3 = new File([file], 'before'+file.name,{type:"image/jpeg"});
 							let url = null;
 							if (window.createObjectURL!=undefined) { // basic
-								url = window.createObjectURL(this.beforefile1) ;
+								url = window.createObjectURL(this.beforefile3) ;
 							}else if (window.webkitURL!=undefined) { // webkit or chrome
-								url = window.webkitURL.createObjectURL(this.beforefile1) ;
+								url = window.webkitURL.createObjectURL(this.beforefile3) ;
 							}else if (window.URL!=undefined) { // mozilla(firefox)
-								url = window.URL.createObjectURL(this.beforefile1) ;
+								url = window.URL.createObjectURL(this.beforefile3) ;
 							}
-							document.getElementById('before').src=url;
+							document.getElementById('before3').src=url;
 							return false;
 						}
 					}else {
@@ -154,7 +229,7 @@
 					})
 				}
 			},
-			after (file) {
+			after1 (file) {
 				if (this.list.state != "treated"){
 					var type = file.name.split('.')
 					if ((type[1] == 'png')||(type[1] == 'gif')||(type[1] == 'jpg')||(type[1] == 'bmp')||(type[1] == 'jpeg')){
@@ -173,7 +248,77 @@
 							}else if (window.URL!=undefined) { // mozilla(firefox)
 								url = window.URL.createObjectURL(this.afterfile1) ;
 							}
-							document.getElementById('after').src=url;
+							document.getElementById('after1').src=url;
+							return false;
+						}
+					}else{
+						this.$Notice.warning({
+							title: this.$t('warning'),
+							desc: this.$t('File type must be picture')
+						})
+					}
+				}else{
+					this.$Notice.warning({
+						title: this.$t('warning'),
+						desc: this.$t('Can not supplementary picture.')
+					})
+				}
+			},
+			after2 (file) {
+				if (this.list.state != "treated"){
+					var type = file.name.split('.')
+					if ((type[1] == 'png')||(type[1] == 'gif')||(type[1] == 'jpg')||(type[1] == 'bmp')||(type[1] == 'jpeg')){
+						if (file.size>2097000) {
+							this.$Notice.warning({
+								title: this.$t('warning'),
+								desc: this.$t('File size must be less than 2M')
+							})
+						}else {
+							this.afterfile2 = new File([file], 'after'+file.name,{type:"image/jpeg"});
+							let url = null;
+							if (window.createObjectURL!=undefined) { // basic
+								url = window.createObjectURL(this.afterfile2) ;
+							}else if (window.webkitURL!=undefined) { // webkit or chrome
+								url = window.webkitURL.createObjectURL(this.afterfile2) ;
+							}else if (window.URL!=undefined) { // mozilla(firefox)
+								url = window.URL.createObjectURL(this.afterfile2) ;
+							}
+							document.getElementById('after2').src=url;
+							return false;
+						}
+					}else{
+						this.$Notice.warning({
+							title: this.$t('warning'),
+							desc: this.$t('File type must be picture')
+						})
+					}
+				}else{
+					this.$Notice.warning({
+						title: this.$t('warning'),
+						desc: this.$t('Can not supplementary picture.')
+					})
+				}
+			},
+			after3 (file) {
+				if (this.list.state != "treated"){
+					var type = file.name.split('.')
+					if ((type[1] == 'png')||(type[1] == 'gif')||(type[1] == 'jpg')||(type[1] == 'bmp')||(type[1] == 'jpeg')){
+						if (file.size>2097000) {
+							this.$Notice.warning({
+								title: this.$t('warning'),
+								desc: this.$t('File size must be less than 2M')
+							})
+						}else {
+							this.afterfile3 = new File([file], 'after'+file.name,{type:"image/jpeg"});
+							let url = null;
+							if (window.createObjectURL!=undefined) { // basic
+								url = window.createObjectURL(this.afterfile3) ;
+							}else if (window.webkitURL!=undefined) { // webkit or chrome
+								url = window.webkitURL.createObjectURL(this.afterfile3) ;
+							}else if (window.URL!=undefined) { // mozilla(firefox)
+								url = window.URL.createObjectURL(this.afterfile3) ;
+							}
+							document.getElementById('after3').src=url;
 							return false;
 						}
 					}else{
@@ -190,15 +335,19 @@
 				}
 			},
 			async finish(val){
-				this.sent=false
+				this.sent=true
 				var formData = new FormData()
 				var formData = new window.FormData()
 				formData.append('file1',this.beforefile1)
-				formData.append('file2',this.afterfile1)
+				formData.append('file2',this.beforefile2)
+				formData.append('file3',this.beforefile3)
+				formData.append('file4',this.afterfile1)
+				formData.append('file5',this.afterfile2)
+				formData.append('file6',this.afterfile3)
 				formData.append('id',this.$route.params.id)
 				formData.append('result',val)
 				let res = await this.$api.finish(formData)
-				this.sent=true
+				this.sent=false
 				if (res.data.code == 0){
 					this.$Notice.success({
 						title: this.$t('success'),
